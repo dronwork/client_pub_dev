90ba41c121e695847227a45b3d8305f5bb298661
Andrei Keida <dronwork@outlook.com>
2020-10-08 20:22:10 +0300

    refactoring

--- lib/widgets/spacing_widgets.dart
+++ lib/widgets/spacing_widgets.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SpaceH10 extends StatelessWidget {
-  const SpaceH10({
-    Key key,
-  }) : super(key: key);
+  const SpaceH10({Key key,}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(height: 10);
-  }
+  Widget build(BuildContext context) => SizedBox(height: 10);
 }
